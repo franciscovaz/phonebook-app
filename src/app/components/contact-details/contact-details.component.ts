4c78f23b29d44cb27bf91a7eb17201e4340185ef
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-24 18:15:13 +0000

    add contact info without api

--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../model/contact';
 
 @Component({
   selector: 'pb-contact-details',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailsComponent implements OnInit {
 
+  contact: Contact;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.contact = new Contact();
+    this.contact.id = 1;
+    this.contact.firstname = 'Francisco';
+    this.contact.lastname = 'Vaz';
+    this.contact.email = 'ftomasvaz@gmail.com';
+    this.contact.phone = '911111111';
+    this.contact.gender = 'Male';
+    this.contact.city = 'Ilhavo';
+    this.contact.state = 'Aveiro';
+    this.contact.country = 'Portugal';
+    this.contact.dob = '1995-06-15';
   }
 
 }
