448527eec7dd3304e591d315efca89b6b3826791
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-29 22:23:16 +0000

    addNewcontact service method

--- src/app/services/phonebook.service.ts
+++ src/app/services/phonebook.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Contact } from '../model/contact';
 
+import 'rxjs/add/operator/map';
+// for up line to work need: npm i rxjs-compat
+
 const baseUrl = 'http://localhost:4300/contacts/';
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,11 @@ export class PhonebookService {
 
   constructor(private http: HttpClient) { }
 
-  getContactDetail(id: number): Observable<any>{
-    return this.http.get(baseUrl + id);
+  getContactDetail(id: number): Observable<Contact>{
+    return this.http.get(baseUrl + id).map(data => data as Contact);
+  }
+
+  addNewContact(contact: Contact): Observable<Contact> {
+    return this.http.post(baseUrl, contact).map(data => data as Contact);
   }
 }
