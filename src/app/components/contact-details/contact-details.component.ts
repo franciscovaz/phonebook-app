6ef0fb5a030306fb39b2b01419705ef725147636
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-03 17:29:58 +0000

    delete contact and navigate with router navigate

--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PhonebookService } from 'src/app/services/phonebook.service';
 import { Contact } from '../../model/contact';
 
@@ -12,16 +12,30 @@ export class ContactDetailsComponent implements OnInit {
 
   contact: Contact = new Contact();
 
-  constructor(private service: PhonebookService, private route: ActivatedRoute) { }
+  constructor(
+    private service: PhonebookService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     // get id from params: this.route.snapshot.params['id']
 
     // another way to get id from params
-    this.route.params.subscribe(routeParams => {
+    this.activatedRoute.params.subscribe(routeParams => {
       this.service.getContactDetail(routeParams['id']).subscribe(response => this.contact = response);
     })
+  }
 
+  deleteContact() {
+    if(!confirm('Are you sure?')){
+      return;
+    }
+
+    this.service.deleteContact(this.contact)
+      .subscribe(() => {
+        this.router.navigate(['/contact-list'])
+      })
   }
 
 }
