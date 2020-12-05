02c4b82c4606b3c9d21d266d09feba02fdccb3ff
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-05 16:01:48 +0000

    add new contact function

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Contact } from 'src/app/model/contact';
+import { PhonebookService } from 'src/app/services/phonebook.service';
 
 @Component({
   selector: 'pb-add-contact',
@@ -10,10 +12,20 @@ export class AddContactComponent implements OnInit {
 
   contact: Contact;
 
-  constructor() { }
+  constructor(private service: PhonebookService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.contact = new Contact();
+  }
+
+  addContact() {
+    this.service.addNewContact(this.contact)
+      .subscribe(contact => {
+        console.log('Added a new contact with id: ' + contact.id);
+        this.router.navigate(['/contact-details', contact.id]);
+      })
   }
 
 }
