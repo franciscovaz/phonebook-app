d9f4cdc0d188d7836b77958e7ac2a8abd706dc82
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-06 14:17:05 +0000

    integrate jquery for pagination scrool

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Contact } from 'src/app/model/contact';
 import { PhonebookService } from 'src/app/services/phonebook.service';
 
+const $ = window['jQuery'];
 @Component({
   selector: 'pb-contact-list',
   templateUrl: './contact-list.component.html',
@@ -17,6 +18,15 @@ export class ContactListComponent implements OnInit {
   ngOnInit(): void {
     this.service.getAllContacts()
       .subscribe(resp => this.contacts = resp);
+
+    $(window).scroll(() => {
+      let w = $(window);
+      let d = $(document);
+
+      if(w.height() + w.scrollTop() === d.height()) {
+        this.loadMore();
+      }
+    })
   }
 
   loadMore() {
